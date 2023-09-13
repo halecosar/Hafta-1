d8fd705b043024c730d62f4c13d36474fb799531
halecosar <halealtunakar@hotmail.com>
2023-09-13 15:39:27 +0300

    Taksimetre_Hesaplayan_Program

--- Taksimetre_Hesaplayan_Program/src/Main.java
+++ Taksimetre_Hesaplayan_Program/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int km;
+        double perKm = 2.20, total, startPrice=10 ;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Km Değerini Griniz : ");
+        km = input.nextInt();
+
+        total = perKm * km;
+        total += startPrice;
+
+        total= total<20 ? 20 : total;
+        System.out.print("Toplam Tutar : " + total );
+
+
+
+
     }
 }
