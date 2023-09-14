b4b35c6cfd794fe4796aa3b8344bb63d3a6ff3a4
halecosar <halealtunakar@hotmail.com>
2023-09-14 18:23:34 +0300

    Artik_Yil_Hesaplama

---
+++ Artik_Yil_Hesaplama/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int year;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        year = input.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            System.out.print(year + " bir artık yıldır !");
+        else System.out.print(year + " bir artık yıldır değildir !");
+    }
+}
